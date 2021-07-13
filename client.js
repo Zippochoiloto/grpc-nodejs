595ea07a0f0f8394fc7c73264a13304db2210677
theanhluong <theanhluong@beeketing.net>
2021-07-13 12:17:03 +0700

    first commit

---
+++ client.js
@@ -0,0 +1,34 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDef = protoLoader.loadSync("todo.proto", {});
+
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+
+const todoPackage = grpcObject.todoPackage;
+const text = process.argv[2];
+
+const client = new todoPackage.Todo("localhost:40000", 
+grpc.credentials.createInsecure())
+
+client.createTodo({
+    "id": -1,
+    "text": text
+}, (err, response) => {
+    console.log("Received from server " + JSON.stringify(response))
+})
+
+client.readTodos({}, (err, response) => {
+    if (err) {
+        console.log("read error", err);
+    }
+    console.log("read the todo from server " + JSON.stringify(response))
+
+})
+
+const call = client.readTodosStream();
+call.on("data", item => {
+    console.log("received item from server " + JSON.stringify(item))
+})
+
+call.on("end", e => console.log("server done!"))
